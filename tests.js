f00e5aeffb0bce09f212892d3dc972c911547cc6
Tom Barker <tommygoggles@gmail.com>
2025-03-19 13:19:24 +0000

    meer

--- tests.js
+++ tests.js
@@ -3,115 +3,50 @@
 
 
 
-let resulttext = "";
 
+async function testpromisething(thing = document.body, name = "requestFullscreen"){
+    let resulttext = "trying it: ";
 
-
-
-class voice{
-    
-    constructor(){
-        this.volume = 1.0;
-        this.setpeople([
-            {
-                voice:null, langs:["en"]
-            },
-            {
-                voice:null, langs:["fr"]
-            }
-        ]);
-    }
-
-    setpeople(people){
-        this.people = [];
-        people.forEach( person=>{
-            this.people.push({
-                pitch:person.pitch || 1.0,
-                rate:person.rate || 1.0,
-                voice:null,
-                langs:person.langs || ["en"]
-            });
-        });
-        //console.log(this.people);
-        this.langs = {};
-        this.people.forEach( person=>{
-            person.langs.forEach( lang=>{
-                this.langs[lang] = {};
-            });
-        });
-
-        if(window.speechSynthesis.getVoices().length===0){
-            window.speechSynthesis.onvoiceschanged = (event)=>{
-                //console.log(event);
-                this.setvoices();
-            }
+    const typer = (Thing, Name=null)=>{
+        if(Name===null){
+            resulttext+="{trying:??}";
         } else {
-            this.setvoices();
+            resulttext+="{trying:?ob?->"+Name+"}";
         }
-    }
-
-    setvoices(){
-        resulttext+=" setting voices.. ";
-        const voices = window.speechSynthesis.getVoices();
-        resulttext+=voices.length.toString()+" ";
-        //console.log(voices);
-        for (const [key, value] of Object.entries(this.langs)) {
-            value.voices = [];
-            for(let voiceno = 0;voiceno<voices.length;voiceno++){
-                if(voices[voiceno].lang.startsWith(key)){
-                    value.voices.push({voiceno});
-                }
-            }
-        }
-        this.people.forEach( person=>{
-            for(let i = 0;i<person.langs.length;i++){
-                const lang = this.langs[person.langs[i]];
-                const count = lang.voices.length;
-                if(count>0){
-                    person.voice = voices[lang.voices[0].voiceno];
-                    break;
-                }
-            }
-        });
-        //console.log(this.people);
-    }
-
-    setgain(newgain){
-        this.volume = newgain;
-        if(window.speechSynthesis.speaking){
-            window.speechSynthesis.cancel();
-            if(this.utterance!==undefined){
-                this.utterance.volume = this.volume;
-                if(newgain>0){
-                    window.speechSynthesis.speak(this.utterance);
-                }
+        const typeofthing = typeof(Thing);
+        resulttext+="<"+typeofthing+">";
+        if(Name!==null){
+            if(typeofthing==="object"){
+                resulttext+="{"+Name+" is "+typeof(Thing[Name])+"}";
+            } else {
+                resulttext+="{NOT AN OBJECT!}";
             }
         }
     }
 
-    say(userandcomment){
-        const person = this.people[userandcomment.user?1:0];
-        this.utterance = new SpeechSynthesisUtterance();
-        this.utterance.text = userandcomment.comment;
-        this.utterance.volume = this.volume;
-        //this.utterance.lang = "en-GB";
-        this.utterance.pitch = person.pitch;
-        this.utterance.rate = person.rate;
-        this.utterance.voice = person.voice;
-        //console.log(this.utterance);
-        if(window.speechSynthesis.speaking){
-            window.speechSynthesis.cancel();
-        }
-        if(this.volume>0){
-            window.speechSynthesis.speak(this.utterance);
-        }
-    }
+    try {
+        typer(thing,name);
+        const result = thing[name]?.();
+        typer(result,"then");
 
+        resulttext+="{awaiting}";
+        const waited = await result;
+        resulttext+="{undefined?}";
+        typer(waited);
+
+    } catch (err) {
+        resulttext+="{catch:"+err.message+"}";
+    }
+    return resulttext;
 }
 
 
 
-const voo = new voice();
+
+
+
+
+
 
 
 
@@ -119,23 +54,26 @@ const result = document.createElement("div");
 result.innerText = "result...";
 document.body.appendChild(result);
 
+async function thingy(){
+    result.innerText = await testpromisething(); 
+}
+
+thingy();
+
 
 const fullscreenbutton = document.createElement("button");
-fullscreenbutton.innerText = "test 1";
+fullscreenbutton.innerText = "mer";
 document.body.appendChild(fullscreenbutton);
 
-fullscreenbutton.onclick = async (e) => {
-    resulttext += "TRYING FS... ";
-    await document.body.requestFullscreen().then( ()=>{
-        resulttext += "said it wo good! ";
-    }).catch(err =>{
-        //fullscreenerror event will trigger
-        resulttext += "OI MATE! ERROR! "+err.message+" ";
-    });
-    result.innerText = resulttext;    
-};
+fullscreenbutton.onclick = thingy;
 
 
+
+
+
+
+
+let resulttext = "";
 
 const speakbutton = document.createElement("button");
 speakbutton.innerText = "test 2";
